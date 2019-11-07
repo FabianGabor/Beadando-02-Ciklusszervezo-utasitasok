345e21dd22797795244a9ef05ab3d2c5f4a75f05
Fábián Gábor <info@fabiangabor.com>
2019-11-07 16:47:01 +0100

    kamat

--- main.c
+++ main.c
@@ -35,6 +35,25 @@ void in_char(int n)
     printf("\n");
 }
 
+void kamat()
+{
+    double toke,kamat;
+    int honap;
+    printf("Toke: ");
+    scanf("%lf", &toke);
+    printf("Kamat (eves): ");
+    scanf("%lf", &kamat);
+    printf("Befektetes ideje (honap): ");
+    scanf("%d", &honap);
+    printf("\nToke \t0/%d \thonap: \t%lf\n", honap,toke);
+
+    double toke_tmp = toke;
+    for (int i=1; i<=honap; i++) {
+        toke_tmp=toke_tmp * (1+(kamat/100/12));
+        printf("Toke \t%d/%d \thonap: \t%lf\n", i,honap,toke_tmp);
+    }
+}
+
 int main()
 {
     // 1. Írassuk ki az első 100 pozitív egész számot, majd írassuk ki azok összegét!
@@ -44,7 +63,11 @@ int main()
     //in_pos_int();
 
     // 3. Folytatás: olvassunk be egy karaktert is, majd írassuk ki annyiszor, mint a beolvasott szám!
-    in_char(in_pos_int());
+    //in_char(in_pos_int());
+
+    // 4. Készíts programot kamatszámításhoz, ciklussal! Kérd be a tőke mennyiségét, a kamat éves értékét (%), illetve a befektetés idejét hónapokban!
+    //    Írd ki hónapról hónapra, hogyan gyarapodik a befektetés!
+    kamat();
 
     return 0;
 }
